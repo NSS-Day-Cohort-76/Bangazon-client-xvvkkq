111bdc819549dec6246367afb75c69e904d9a71e
Brit Beaty <britneybeaty@gmail.com>
2025-07-18 13:20:53 -0500

    Added next.config.js and implemented logic to render product images

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['localhost'], // Add any other domains as needed
+  },
+}
+
+module.exports = nextConfig
