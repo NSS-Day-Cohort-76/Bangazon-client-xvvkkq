25f6ddb099cbe49159b590b6be7d14b1a1427374
Tyler Martin <tizzlerrm713@gmail.com>
2025-08-01 14:04:08 -0500

    the test for reviews and ratings working

--- components/rating/card.js
+++ components/rating/card.js
@@ -1,20 +1,27 @@
-import { Rating } from 'react-simple-star-rating'
+import dynamic from "next/dynamic";
+
+// Import Rating component without SSR to avoid window errors
+const Rating = dynamic(
+  () => import("react-simple-star-rating").then((mod) => mod.Rating),
+  {
+    ssr: false,
+    loading: () => <div>⭐</div>,
+  }
+);
 
 export function RatingCard({ rating }) {
   return (
     <div className="tile is-child">
       <article className="media box is-align-items-center">
         <figure className="media-left">
-          <Rating initialValue={rating.score} readonly={true} />
+          <Rating initialValue={rating.rating} readonly={true} />
         </figure>
         <div className="media-content">
           <div className="content">
-            <p>
-              {rating.review}
-            </p>
+            <p>{rating.review}</p>
           </div>
         </div>
       </article>
     </div>
-  )
+  );
 }
